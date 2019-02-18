9ef1eb4c3c7230db2a42b0968dd1d148be9521d5
ramos07 <ramosricardo96@gmail.com>
2019-02-18 12:32:12 -0800

    Create RelayCommand.cs

---
+++ Lab_Assignment_3/Lab_Assignment_3/Command/RelayCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lab_Assignment_3.Command
+
+{
+    public class RelayCommand : ICommand
+    {
+
+        Action<object> _execute;
+        Func<object, bool> _canexecute;
+        public RelayCommand(Action<object> execute, Func<object, bool> canexecute)
+        {
+            _execute = execute;
+            _canexecute = canexecute;
+
+        }
+        public bool CanExecute(object parameter)
+        {
+            if (_canexecute != null)
+            {
+                return _canexecute(parameter);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
