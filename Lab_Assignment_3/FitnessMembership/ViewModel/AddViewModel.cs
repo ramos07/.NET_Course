e99d7cc01fecef9a8a80205036f3fd6597a9fdcf
ramos07 <ramosricardo96@gmail.com>
2019-02-19 15:53:59 -0800

    Create AddViewModel.cs

---
+++ Lab_Assignment_3/FitnessMembership/ViewModel/AddViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FitnessMembership.View;
+using Prism.Mvvm;
+using System.Windows.Input;
+using Prism.Commands;
+using System.Windows.Data;
+
+
+namespace FitnessMembership.ViewModel
+{
+    class AddViewModel 
+    {
+    }
+}
