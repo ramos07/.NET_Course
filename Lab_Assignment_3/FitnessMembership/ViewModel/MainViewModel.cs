61b6c23cc95563cc59dbe14d4f42806126af362d
ramos07 <ramosricardo96@gmail.com>
2019-02-20 20:05:52 -0800

    Update MainViewModel.cs

--- Lab_Assignment_3/FitnessMembership/ViewModel/MainViewModel.cs
+++ Lab_Assignment_3/FitnessMembership/ViewModel/MainViewModel.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using FitnessMembership.Model;
+
+//ReceiverViewModel.cs
 
 namespace FitnessMembership.ViewModel
 {
-    class MainViewModel
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
     {
+        private string _firstName;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                _firstName = value;
+                RaisePropertyChanged("FirstName");
+            }
+        }
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+            Messenger.Default.Register<Member>(this, OnReceiveMessageAction);
+        }
+        private void OnReceiveMessageAction(Member obj)
+        {
+            FirstName = obj.FirstName;
+        }
     }
 }
