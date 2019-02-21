5e8f9f102ec5099c1778ba0392eccb340d471222
ramos07 <ramosricardo96@gmail.com>
2019-02-20 20:05:46 -0800

    Update AddViewModel.cs

--- Lab_Assignment_3/FitnessMembership/ViewModel/AddViewModel.cs
+++ Lab_Assignment_3/FitnessMembership/ViewModel/AddViewModel.cs
@@ -3,16 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using FitnessMembership.View;
-using Prism.Mvvm;
-using System.Windows.Input;
-using Prism.Commands;
-using System.Windows.Data;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using FitnessMembership.Model;
 
+//SenderViewModel.cs
 
 namespace FitnessMembership.ViewModel
 {
-    class AddViewModel 
+    class AddViewModel : ViewModelBase
     {
+        private string _firstNameText;
+        public RelayCommand OnClickCommand { get; set; }
+
+        public string FirstNameText
+        {
+            get
+            {
+                return _firstNameText;
+            }
+            set
+            {
+                _firstNameText = value;
+                RaisePropertyChanged("FirstNameText");
+            }
+        }
+        public AddViewModel()
+        {
+            OnClickCommand = new RelayCommand(OnClickCommandAction, null);
+        }
+        
+        private void OnClickCommandAction()
+        {
+            var viewFirstName = new Member()
+            {
+                FirstName = FirstNameText
+            };
+            Messenger.Default.Send(viewFirstName);
+        }
     }
+   
 }
