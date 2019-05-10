70b006550669e669378f072078a3f0e66c21fb81
ramos07 <ramosricardo96@gmail.com>
2019-05-10 13:50:14 -0700

    Create TodoContext83.cs

---
+++ Lab_Assignment_7/TodoApi83/TodoApi83/Models/TodoContext83.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi83.Models
+{
+    public class TodoContext83 : DbContext
+    {
+        public TodoContext83(DbContextOptions<TodoContext83> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem83> TodoItems { get; set; }
+    }
+}
